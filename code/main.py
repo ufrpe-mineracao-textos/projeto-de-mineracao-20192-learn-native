06368ab09411c4ac944dce178fe143e118c9d95a
Tiagoblima <tiago.blima@ufrpe.br>
2019-10-19 14:12:47 -0300

    update

--- code/main.py
+++ code/main.py
@@ -2,6 +2,10 @@ from util import util
 import os
 import re
 import math
+import string
+import nltk
+from nltk.tokenize import RegexpTokenizer
+
 # --- Tirando referências -----
 
 
@@ -19,52 +23,79 @@ signature = {}
 letter_freq = {}
 raw_text = ' '.join(scripture).lower()
 
-for verse in scripture:
+tokenizer = RegexpTokenizer(r'\w+', flags=re.UNICODE)
 
-    tokens = verse.split()
+tokens = tokenizer.tokenize(raw_text)
 
-    for token in tokens:
-        for l in token:
+SIZE = len(tokens)
+STEP = 1/100
+percent = 0 
+temp = 0
+
+for token in tokens:
+
+    if temp is int(SIZE*STEP):
+        percent += 1
+        temp = 0
+        print('#', end='')
+
+    for l in token:
+
+        try:
+            freq = letter_freq.get(l.lower())
+            freq += 1
+            letter_freq[l.lower()] = freq
+        
+        except TypeError:
+            
+            freq = 1
+            letter_freq[l.lower()] = freq
+    
+    for size in range(1, 6):
+
+        if len(token) > size+3:
+            sufix = token[-size:]
+            
             try:
-                freq = letter_freq.get(l.lower())
+                freq = sufix_freq.get(sufix)
                 freq += 1
-                letter_freq[l.lower()] = freq
-            
+                sufix_freq[sufix] = freq
             except TypeError:
-                
                 freq = 1
-                letter_freq[l.lower()] = freq
-      
-        for size in range(1, 6):
-
-            if len(token) > size+3:
-                sufix = token[-size:]
+                sufix_freq[sufix] = freq
+    temp += 1
                 
-                try:
-                    freq = sufix_freq.get(sufix)
-                    freq += 1
-                    sufix_freq[sufix] = freq
-                except TypeError:
-                    freq = 1
-                    sufix_freq[sufix] = freq
-
-               
-
-   
-            
 keys = list(letter_freq.keys())
 print(keys[9], letter_freq[keys[0]])
 
 print('Stemming words: ', end='#')
-SIZE = len(sufix_freq.keys())
-temp = 1
-for sufix in list(sufix_freq.keys()):
-    
-    
-    print('#', end='')
 
-    search = re.search(r'\w+'+str(sufix), raw_text)
-    
+temp = 1
+
+
+raw_text = ' '.join(tokenizer.tokenize(raw_text))
+
+sufixes = ' '.join(sufix_freq.keys())
+
+sufixes = tokenizer.tokenize(sufixes)
+
+SIZE = len(sufixes)
+STEP = 1/100
+percent = 0 
+temp = 0
+for sufix in sufixes:
+
+    if temp is int(SIZE*STEP):
+        percent += 1
+        temp = 0
+        print('#', end='')
+        
+    try:
+        search = re.findall(r'\w+'+str(sufix), raw_text)
+    except re.error:
+        print("\n Sufixo: ", sufix)
+        breakpoint()
+
     try:
         for word in set(search.group(0)):
             stem = word.lower().replace(sufix, '')
--- code/util/preprocess.py
+++ code/util/preprocess.py
@@ -1,30 +1,85 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.tokenize import RegexpTokenizer
+from util import util
 import pandas as pd
 import numpy as np
 import os
+import re
+import math
+import string
+import nltk
 
-path = '../'
-data_list = os.listdir(path)
 
 
-data = pd.read_csv(path + 'text-label.csv', encoding='utf8', index_col=False)
 
-data_text = data['text']
-data_label = data['label'].drop_duplicates()
-print("Labels ", data_label[0])
-print('Text sample: ', data_text[0])
+def count_words():
+    path = '../'
+    data_list = os.listdir(path)
 
-# Applying count vectorizer
-text = data[data['label'] == data_label[0]]['text']
 
-count = CountVectorizer()
-count_vec = count.fit_transform(text.values.astype('U'))
-print("Counting shape: ", count_vec.shape)
+    data = pd.read_csv(path + 'text-label.csv', encoding='utf8', index_col=False)
 
-# Applying TF_IDF transform
+    data_text = data['text']
+    data_label = data['label'].drop_duplicates()
+    print("Labels ", data_label[0])
+    print('Text sample: ', data_text[0])
 
-tf_idf = TfidfTransformer()
-tf_idf_vec = tf_idf.fit_transform(count_vec)
-print("TF-IDF shape: ", tf_idf_vec.shape)
+    # Applying count vectorizer
+    text = data[data['label'] == data_label[0]]['text']
 
+    count = CountVectorizer()
+    count_vec = count.fit_transform(text.values.astype('U'))
+    print("Counting shape: ", count_vec.shape)
+
+    # Applying TF_IDF transform
+
+    tf_idf = TfidfTransformer()
+    tf_idf_vec = tf_idf.fit_transform(count_vec)
+    print("TF-IDF shape: ", tf_idf_vec.shape)
+
+
+class autoStemm:
+
+    path_dir = ''
+    sufix_freq = {}
+    letter_freq = {}
+
+def freq_counter(self, text):
+
+    
+    raw_text = ' '.join(text).lower()
+
+    tokenizer = RegexpTokenizer(r'\w+', flags=re.UNICODE)
+    tokens = tokenizer.tokenize(raw_text)
+
+    for token in tokens:
+
+        for l in token:
+
+            try:
+                freq = self.letter_freq.get(l.lower())
+                freq += 1
+                self.letter_freq[l.lower()] = freq
+            
+            except TypeError:
+                
+                freq = 1
+                self.letter_freq[l.lower()] = freq
+
+        for size in range(1, 6):
+
+            if len(token) > size+3:
+                sufix = token[-size:]
+                
+                try:
+                    freq = self.sufix_freq.get(sufix)
+                    freq += 1
+                    self.sufix_freq[sufix] = freq
+                except TypeError:
+                    freq = 1
+                    self.sufix_freq[sufix] = freq
+
+    def auto_stemm(self):
+
+        signature = {}
