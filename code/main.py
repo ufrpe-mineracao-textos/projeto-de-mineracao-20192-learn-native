19334d99594bc3b69ac7a97189df7102949f1909
Tiago de Lima <tiago.blima02@gmail.com>
2020-05-06 19:52:11 -0300

    fixing errors

--- code/main.py
+++ code/main.py
@@ -1,23 +1,14 @@
-import math
-import os, re
-import threading
-from sys import intern
+import os
+import time
 
-import numpy as np
 import pandas as pd
-import sys
-from util.preprocess import TextPreprocess, AutoStem, stem_words
-from language_clf import LangClf
-from util.util import get_tokens
-import pprint
-from nltk import RegexpTokenizer
+import matplotlib.pyplot as plt
+from language_clf import LangClf, Imag_path
 
 # --- Tirando referências -----
 
 path = r'../Resources/bibles/'
 stems_path = r'../Resources/stems/stems.csv'
-
-PATH = r'../out/table'
 
 
 def load_data(threshold=4):
@@ -48,33 +39,58 @@ def classify(threshold=4):
     Makes the classification of all languages according to a threshold
     :return: two vectors with the text label, predicted, similarity of all predictions
     """
+    start = time.time()  # initial time
 
-    print("Loading data...")
     stems_dic, trains, testes = load_data(threshold)  # Loads the data according to the established threshold in
     # terms of number of books
-    print("Load done")
 
     clf = LangClf(stems_dic)
-    print("Fitting the Classifier...")
+
     clf.fit(trains, testes)  # Fits the Classifier with the training and test set
 
     # clf.load_clf(pd.read_csv('top_ranked_words.csv', encoding='utf8'))
-    print("Testing...")
+
     results = clf.test()
+
+    time_taken = (time.time() - start)
+    mean = clf.get_mean_similarity()
+    std = clf.get_std_similarity()
+    accuracy = clf.get_accuracy()
+    train_mean_size = clf.get_train_mean_size()
+
+    print("Threshold: ", threshold)
+    print("Mean similarity: {:.5f}".format(mean))
+    print("Standard Deviation similarity: {:.5f}".format(std))
+    print("Accuracy: ", accuracy)
+    print("Mean train size: {:.5f} words".format(train_mean_size))
+    print("Final time: {:.5f}secs".format(time_taken))
     print(results)
+    print()
+    print('-' * 40)
+    print()
+    # clf.get_test_plot()
+    return {
+        "mean": mean,
+        "std": std,
+        "accuracy": accuracy,
+        "train_mean_size": train_mean_size,
+        "time_taken": time_taken
+    }
 
-    print("Mean similarity: ", clf.get_mean_similarity())
-    print("Standard Deviation similarity: ", clf.get_std_similarity())
-    print("Accuracy: ", clf.get_accuracy())
-    print("Mean train size: ", clf.get_train_mean_size())
 
-    clf.get_test_plot()
-    return sorted(results.values(), key=lambda tup: tup[1])
-
-
+# plt.title("Mean Match rate Evolution")
+# plt.xlabel("Number of books")
+# plt.ylabel("Mean Match rate")
+# plt.savefig(Imag_path + 'mean.pdf', dpi=600)
 def main():
+    results_tup = []
     for i in range(1, 5):
-        classify(i)
+        result = classify(i)
+        results_tup.append((i, result["mean"], result["std"],
+                            result["accuracy"], result["train_mean_size"],
+                            result['time_taken']))
+    y_s = [tup[1] for tup in results_tup]
+    x_s = [str(tup[0]) for tup in results_tup]
 
 
 if __name__ == "__main__":
